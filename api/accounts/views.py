657e70ba335bc0c8cf06c60958c94d6563251420
guilherme <guiegle@hotmail.com>
2025-05-27 21:03:10 -0300

    feature: atualiza estruturas de usuario e sticker

--- api/accounts/views.py
+++ api/accounts/views.py
@@ -26,10 +26,11 @@ class RegisterView(APIView):
         username = request.data.get("username")
         password = request.data.get("password")
         email = request.data.get("email")
+        picture = request.data.get("picture")
         if not username or not password or not email:
             return Response({"error": "Preencha todos os campos"}, status=status.HTTP_400_BAD_REQUEST)
 
-        user = User.objects.create_user(username=username, password=password, email=email)
+        user = User.objects.create_user(username=username, password=password, email=email, picture=picture)
         Album.objects.create(user_id=user.id)
         return Response({"message": "Usuário criado com sucesso!"}, status=status.HTTP_201_CREATED)
 
--- api/stickers/models.py
+++ api/stickers/models.py
@@ -15,13 +15,13 @@ class Sticker(models.Model):
         (SHIELD, 'Shield'),
     ]
 
-    number = models.PositiveIntegerField(default=0)  # Positive integer, auto-incremented
+    text = models.CharField(max_length=50, default="")
     name = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
     type = models.CharField(max_length=10, choices=TYPE_CHOICES)
     sticker_group = models.ForeignKey(StickerGroup, on_delete=models.CASCADE, default=1)
     album = models.ForeignKey(Album, on_delete=models.CASCADE, default=1)
-    amount = models.PositiveIntegerField(default=0)  # Default to 0 for unsigned integers
+    amount = models.PositiveIntegerField(default=0)
 
 
     def __str__(self):
